b2a09ad661e86be8b4b50f85e7ac2dcd70dcb368
Kyrylo_Yefremov <kyrylo.yefremov@tul.cz>
2024-10-05 22:43:44 +0200

    add multiple threads processing

--- cv01/scraper.py
+++ cv01/scraper.py
@@ -1,61 +1,74 @@
 import json
-from pprint import pprint
-
 import requests
 
 from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
 
 from cookies_parser import parse_cookie_file
 
 
 URL = "https://www.idnes.cz/"
+OUTPUT_FILENAME = 'idnes-data.json'
+MAX_THREADS = 20
+ARTICLES_NUM = 25000
 
 
-def parse_info(href: str, cookies: dict, links_to_parse: list[str]) -> dict | None:
-    if href[:4] != 'http':
+def parse_info(href: str, cookies: dict, links_to_parse: list[str], processed_links: set) -> dict | None:
+    # Skip already processed links
+    if href in processed_links:
         return None
 
-    # print(href)
-    response = requests.get(href, cookies=cookies)
-
-    soup = BeautifulSoup(response.text, 'html.parser')
-
-    # In case of not valid page - skipping
-    try:
-        title = soup.find('h1').text
-    except AttributeError:
+    # Skip links that are not articles
+    if not href.startswith('http'):
         return None
 
-    # Getting the whole text of the article
-    text = ''
-    for paragraph in soup.find_all('p'):
-        text += paragraph.text + '\n'
-
-    img_count = len(soup.find_all('img'))
-    categories = [a.text for a in soup.find_all('a', class_='b')]
-
     try:
-        date = soup.find('span', class_='time-date').get('content')
-    except AttributeError:
-        date = "NULL"
+        response = requests.get(href, cookies=cookies)
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    li_tag = soup.find('li', class_='community-discusion')
-    comments_num = int(li_tag.find('span').text.split()[0][1:]) if li_tag else 0
+        # In case of not valid page - skipping
+        try:
+            title = soup.find('h1').text
+        except AttributeError:
+            return None
+        if title == "Example Domain":
+            return None
 
-    result = {
-        'title': title,
-        'text': text,
-        'img_count': img_count,
-        'categories': categories,
-        'date': date,
-        'comments_num': comments_num,
-        'link': href
-    }
+        # Getting the whole text of the article
+        text = ''
+        for paragraph in soup.find_all('p'):
+            text += paragraph.text + '\n'
 
-    articles_to_parse = soup.find_all('a', class_='art-link')
-    links_to_parse += [article['href'] for article in articles_to_parse]
+        img_count = len(soup.find_all('img'))
+        categories = [a.text for a in soup.find_all('a', class_='b')]
 
-    return result
+        try:
+            date = soup.find('span', class_='time-date').get('content')
+        except AttributeError:
+            date = "NULL"
+
+        li_tag = soup.find('li', class_='community-discusion')
+        comments_num = int(li_tag.find('span').text.split()[0][1:]) if li_tag else 0
+
+        result = {
+            'title': title,
+            'text': text,
+            'img_count': img_count,
+            'categories': categories,
+            'date': date,
+            'comments_num': comments_num,
+            'link': href
+        }
+
+        # Find all new links to other articles on the page and add them to the main list
+        new_links = {article['href'] for article in soup.find_all('a', class_='art-link')}
+        links_to_parse += [link for link in new_links if link not in processed_links]
+
+        return result
+
+    except requests.RequestException as rexception:
+        print(f"Request failed: {rexception}")
+        return None
 
 
 def main():
@@ -66,30 +79,41 @@ def main():
     # with open('idnes.html', 'w', encoding='utf-8') as file:
     #     file.write(soup.prettify())
 
-    # Parsing the main page: finding all articles
-    articles_to_parse = soup.find_all('a', class_='art-link')
+    # Parsing the main page: finding all articles and links to other articles
+    articles_to_parse = [article['href'] for article in soup.find_all('a', class_='art-link')]
     if len(articles_to_parse) == 0:
         raise Exception('No articles found')
-    links_to_parse = [article['href'] for article in articles_to_parse]
 
     # Parsing the articles
     outputs = []
-    for link in links_to_parse:
-        json_output = parse_info(href=link, cookies=cookies, links_to_parse=links_to_parse)
+    processed_links = set()
 
-        # In case of not valid page - skipping
-        if not json_output:
-            continue
+    while len(outputs) < ARTICLES_NUM:
+        # Parsing the articles in parallel: use multiple threads (max_workers) to send requests
+        with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
+            # Creating a list of tasks for the executor
+            # submit() sends a single task to another thread and returns a Future object
+            # that represents the result of the <parse_info> function
+            futures = [executor.submit(
+                parse_info, link, cookies, articles_to_parse, processed_links
+            ) for link in articles_to_parse]
 
-        outputs.append(json_output)
-        print(len(outputs))
-        if len(outputs) == 130000:
-            break
+            for future in futures:
+                json_result = future.result()
+                if json_result:
+                    outputs.append(json_result)
+                    processed_links.add(json_result['link'])
+                    print(len(outputs))
 
     # Saving the parsed data to a file
-    with open('idnes-data.json', 'w', encoding='utf-8') as file:
+    with open(OUTPUT_FILENAME, 'w', encoding='utf-8') as file:
         json_to_write = json.dumps(outputs, ensure_ascii=False, indent=2)
         file.write(json_to_write)
+
+    print("Done\n" + "=" * 100)
+    sss = set([res['link'] for res in outputs])
+    print(len(outputs))
+    print(len(sss))
 
 
 if __name__ == '__main__':
