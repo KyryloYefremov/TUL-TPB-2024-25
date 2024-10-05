918708dea30156314d4c3e064e1087b0ecbeb165
Kyrylo_Yefremov <kyrylo.yefremov@tul.cz>
2024-10-05 21:44:27 +0200

    add json reader to print scraper output

---
+++ cv01/json_reader.py
@@ -0,0 +1,30 @@
+import json
+
+from scraper import OUTPUT_FILENAME
+
+
+def read_scraper_output_json(json_file):
+    with open(json_file, 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+
+def print_json(json_output):
+    """
+    Prints the first 5 articles from the json_output
+    """
+    for article in json_output[:5]:
+        print('-' * 200 + '\n')
+        print(
+            f"Title: {article['title']}\n"
+            f"Date: {article['date']}\n"
+            f"Categories: {', '.join(article['categories'])}\n"
+            f"Text: {article['text'][:100]}...\n"
+            f"Image count: {article['img_count']}\n"
+            f"Comments: {article['comments_num']}\n"
+        )
+
+
+if __name__ == '__main__':
+    json_output = read_scraper_output_json(OUTPUT_FILENAME)
+    print_json(json_output= json_output)
+
