089ac27d7d4dff87b38ae7a5f0e7c7eab903f073
Kyrylo_Yefremov <kyrylo.yefremov@tul.cz>
2024-10-13 20:05:57 +0200

    Done bonus tasks 1-4

--- cv02/analyzer.py
+++ cv02/analyzer.py
@@ -21,10 +21,7 @@ def preprocess(art_list: list[dict]) -> pd.DataFrame:
     return df
 
 
-def main():
-    articles_list = read_json(INPUT_FILENAME)
-    df = preprocess(articles_list)
-
+def main_task(df: pd.DataFrame):
     # print(df.columns)
     # ['title', 'text', 'img_count', 'categories', 'date', 'comments_num', 'link']
 
@@ -90,16 +87,59 @@ def main():
     print(f"10. Total number of words: {texts_word_num}, {texts_word_counter.__len__()}")
 
 
+def bonus_task(df: pd.DataFrame):
+    long_words_counter = Counter()
+    covid19_word_counter = Counter()
+    word_counter = Counter()
+    average_words_length_sum = 0
+    words_count = 0
     
 
+    for i, text in enumerate(df.text):
+        # 1
+        words = re.findall(r'\b\w{6,}+\b', text.lower())
+        long_words_counter.update(words)
+
+        # 2
+        covid19_count = len(re.findall(r'\bcovid[-]?19\b', text.lower()))
+        covid19_word_counter[df.title[i]] = covid19_count
+
+        # 3
+        words_count = len(re.findall(r'\b\w+\b', text.lower()))
+        word_counter[df.title[i]] = words_count
+
+        # 4
+        words_length = [len(word) for word in words]
+        average_words_length_sum += sum(words_length) / len(words_length)
 
 
 
-    
+    ### 1. Printing 8 most frequent words in articles (min 6 signs length)
+    print("1. Articles' 8 most frequent words 6+ letters:")
+    for (w, n) in long_words_counter.most_common(8):
+        print(f"\t{w} - {n}")
 
+    ### 2. Printing 3 atricles with the highest frequency of the word Covid-19
+    print("2. Articles with most frequent appearence of 'Covid-19':")
+    for title, num in covid19_word_counter.most_common(3):
+        print(f"\t'{title}' = {num}")
 
+    ### 3. Printing articles with maximum and minimum words count
+    most = word_counter.most_common()
+    max_words_article = most[0]
+    min_words_article = most[-1]
+    print(f"3. Atricle with max word count: \n\t'{max_words_article[0]}' = {max_words_article[1]}")
+    print(f"   Atricle with min word count: \n\t'{min_words_article[0]}' = {min_words_article[1]}")
+
+    ### 4. Printing average word length
+    average_wl = round(average_words_length_sum / df.title.size)
+    print(f"4. Average word length: {average_wl}")
 
 
 
 if __name__ == "__main__":
-    main()
+    articles_list = read_json(INPUT_FILENAME)
+    df = preprocess(articles_list)
+    # main_task(df)
+    print("=" * 100, "\nBONUS TASKS\n")
+    bonus_task(df)
