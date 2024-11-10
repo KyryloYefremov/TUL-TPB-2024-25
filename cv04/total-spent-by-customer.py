6a3534049f3009c58fcf835bed443fcb110aeb89
Kyrylo_Yefremov <kyrylo.yefremov@tul.cz>
2024-11-10 16:13:36 +0100

    Done task 3: add module to count spendings for every customer

---
+++ cv04/total-spent-by-customer.py
@@ -0,0 +1,28 @@
+from pyspark import SparkConf, SparkContext
+from config import MASTER
+
+conf = SparkConf().setMaster(MASTER).setAppName("RatingsHistogram")
+# conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
+sc = SparkContext(conf = conf)
+
+lines = sc.textFile("/files/customer-orders.csv")
+
+
+def parseInfo(line):
+    fields = line.split(',')
+    customerID = fields[0]
+    money = float(fields[2])
+    return (customerID, money)
+
+
+parsedLines = lines.map(parseInfo)
+
+# Count all customer spendings as sum
+customerSpendings = parsedLines.reduceByKey(lambda x, y: x + y)
+# Revert and sort in RDD
+customerSpendingsSorted = customerSpendings.map(lambda x: (x[1], x[0])).sortByKey(ascending=False)
+
+results = customerSpendingsSorted.collect()
+
+for result in results:
+    print(result)
