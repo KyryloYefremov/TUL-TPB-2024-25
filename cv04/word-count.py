e8aca0cb9c7b513004256da9497e55dd495e92a8
Kyrylo_Yefremov <kyrylo.yefremov@tul.cz>
2024-11-10 16:00:21 +0100

    Add text processing. Add sorting

--- cv04/word-count.py
+++ cv04/word-count.py
@@ -1,14 +1,27 @@
+import re
 from pyspark import SparkConf, SparkContext
+from config import *
 
-conf = SparkConf().setMaster("spark://fa367db42f31:7077").setAppName("WordCount")
+conf = SparkConf().setMaster(MASTER).setAppName("WordCount")
 # conf = SparkConf().setMaster("local").setAppName("WordCount")
 sc = SparkContext(conf = conf)
 
-input = sc.textFile("/files/book.txt")
-words = input.flatMap(lambda x: x.split())
-wordCounts = words.countByValue()
+inputText = sc.textFile("/files/book.txt")
+# Remove special symbols and convert to lowcase
+lines = inputText.flatMap(lambda line: re.findall(r'\b\w+\b', line.lower()))
 
-for word, count in wordCounts.items():
-    cleanWord = word.encode('ascii', 'ignore')
-    if (cleanWord):
-        print(cleanWord.decode() + " " + str(count))
+words = lines.flatMap(lambda x: x.split())
+# Change to (word, count) structure
+wordCounts = words.map(lambda x: (x, 1)).reduceByKey(lambda x, y: x + y)
+# Revert to (count, word) and sort by key
+wordCountsSorted = wordCounts.map(lambda x: (x[1], x[0])).sortByKey(ascending=False)
+
+results = wordCountsSorted.collect()
+
+for result in results[:20]:
+    count = str(result[0])
+    word = result[1].encode('ascii', 'ignore')
+    if word:
+        print(word.decode() + ":\t\t\t" + count)
+
+
